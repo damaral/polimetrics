11fdf68b9b67f27dba4bfe26e41589c75ca22e56
Daniel A. Sousa <danpcnux@gmail.com>
2013-11-23 19:43:25 -0200

    heroku de novo

--- config/initializers/paperclip.rb
+++
@@ -1,2 +0,0 @@
-#Paperclip::Attachment.default_options[:url] = ':inov.s3-website-us-east-1.amazonaws.com'
-#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
